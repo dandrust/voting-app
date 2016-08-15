125f50a6345966c8d8c746cc2e4b7cda170ac7ec
Dan <dandrust@gmail.com>
2016-08-15 23:37:35 +0000

    change port for heroku

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use(flash());
 require('./app/routes.js')(app, passport);
 
 
-app.listen(8080, function(){
-    console.log("Listening on port 8080");
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Listening");
 });
--- test.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://test:test@ds139665.mlab.com:39665/dandrust-url');
-var User = require('./models/user');
-
-User.findOne({username: 'the_big_tester'}, function(err, user){
-    if (err) throw err;
-    console.log(user);
-    user.greet();
-});
